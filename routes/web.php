6062fb91feda1969c234d7ff894e4cc58dceebb4
Waldo Fernandez <waldocu02@gmail.com>
2025-03-09 14:03:32 -0400

    feat: add register endpoint

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +12,7 @@ Route::get('/', function () {
 Route::prefix('auth')->group(function(){
     Route::post('/login', LoginController::class);
     Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
 
 
