00e50da04ed03b1694b56a2fbd0746b8dd122c3a
Waldo Fernandez <waldocu02@gmail.com>
2025-03-09 13:30:51 -0400

    feat: add SPA Login endpoint

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $user= User:: where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            Throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    }
+}
