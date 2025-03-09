665d42f499c24e578c56d50b8557fbe87258874e
Waldo Fernandez <waldocu02@gmail.com>
2025-03-09 17:41:49 -0400

    feat: protect the API routes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -16,9 +17,15 @@ class LoginController extends Controller
      */
     public function __invoke(LoginRequest $request)
     {
-        $user= User:: where('email', $request->email)->first();
+       /*  $user= User:: where('email', $request->email)->first();
 
         if(!$user || !Hash::check($request->password, $user->password)){
+            Throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        } */
+
+        if(!Auth::attempt($request->only('email', 'password'))){
             Throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
