96b35469a8f8426b0d6d8e6301f8a5722a691913
Waldo Fernandez <waldocu02@gmail.com>
2025-03-09 18:26:17 -0400

    feat: implement sanctum for API token authentication

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,19 @@ use App\Http\Controllers\Api\v1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\CompleteTaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 
 Route::middleware('auth:sanctum')->prefix('v1') -> group(function() {
     Route::apiResource('/tasks', TaskController::class); // GET, POST, PUT, DELETE
     Route::patch('/tasks/{task}/complete',CompleteTaskController::class); // PATCH
+});
+
+Route::prefix('auth')->group(function(){
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
 });
 
 Route::get('/user', function (Request $request) {
